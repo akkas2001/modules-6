828afe4083171d3d3b0d3fb0adb39bfab765ad31
akkas2001 <mdakkasali400298@gmail.com>
2023-09-03 22:38:33 +0600

    cann	 slove problem

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const multer = require("multer");
+
+
+
+
+
+app.listen(8000,function(){
+    console.log("server is running.");
+})
+
+
+const App = require("app");
